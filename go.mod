99089790b53c7d4824e0a463de45f51c317eabae
zhengjianxun <zhuimengshaonian04@gmail.com>
2022-07-23 00:30:55 +0800

    encoding xxx -> utf8

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/dreamerjackson/crawler
 
 go 1.18
+
+require (
+	golang.org/x/net v0.0.0-20220708220712-1185a9018129 // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
--- main.go
+++ main.go
@@ -1,45 +1,56 @@
 package main
 
 import (
-	"bytes"
+	"bufio"
 	"fmt"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
 	"io/ioutil"
 	"net/http"
-	"strings"
 )
 
 func main() {
-	url := "https://www.thepaper.cn/"
-	resp, err := http.Get(url)
-
-	if err != nil {
-		fmt.Println("fetch url error:%v", err)
-		return
-	}
-
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		fmt.Printf("Error status code:%v", resp.StatusCode)
-	}
-
-	body, err := ioutil.ReadAll(resp.Body)
+	url := "https://www.chinanews.com.cn/"
+	body, err := Fetch(url)
 
 	if err != nil {
 		fmt.Println("read content failed:%v", err)
 		return
 	}
 
-	numLinks := strings.Count(string(body), "<a")
-	fmt.Printf("homepage has %d links!\n", numLinks)
+	fmt.Println(string(body))
+}
 
-	numLinks = bytes.Count(body, []byte("<a"))
-	fmt.Printf("homepage has %d links!\n", numLinks)
+func Fetch(url string) ([]byte, error) {
 
-	exist := strings.Contains(string(body), "疫情")
-	fmt.Printf("是否存在疫情:%v\n", exist)
+	resp, err := http.Get(url)
 
-	exist = bytes.Contains(body, []byte("疫情"))
-	fmt.Printf("是否存在疫情:%v\n", exist)
+	if err != nil {
+		panic(err)
+	}
 
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("Error status code:%d", resp.StatusCode)
+	}
+	bodyReader := bufio.NewReader(resp.Body)
+	e := DeterminEncoding(bodyReader)
+	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
+	return ioutil.ReadAll(utf8Reader)
+}
+
+func DeterminEncoding(r *bufio.Reader) encoding.Encoding {
+
+	bytes, err := r.Peek(1024)
+
+	if err != nil {
+		fmt.Println("fetch error:%v", err)
+		return unicode.UTF8
+	}
+
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
 }

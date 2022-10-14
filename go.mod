42792c2805cdb66cda100dbe68c0b2a7d3fa88e0
zhengjianxun <zhuimengshaonian04@gmail.com>
2022-10-15 00:28:06 +0800

    log

--- go.mod
+++ go.mod
@@ -7,7 +7,15 @@ require (
 	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/antchfx/htmlquery v1.2.5 // indirect
 	github.com/antchfx/xpath v1.2.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.8.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.23.0 // indirect
 	golang.org/x/net v0.0.0-20220708220712-1185a9018129 // indirect
 	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,19 +1,23 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
-	"github.com/PuerkitoBio/goquery"
 	"github.com/dreamerjackson/crawler/collect"
+	"github.com/dreamerjackson/crawler/log"
 	"github.com/dreamerjackson/crawler/proxy"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 	"time"
 )
 
 func main() {
+	plugin, c := log.NewFilePlugin("./log.txt", zapcore.InfoLevel)
+	defer c.Close()
+	logger := log.NewLogger(plugin)
+	logger.Info("log init end")
 	proxyURLs := []string{"http://127.0.0.1:8888", "http://127.0.0.1:8888"}
 	p, err := proxy.RoundRobinProxySwitcher(proxyURLs...)
 	if err != nil {
-		fmt.Println("RoundRobinProxySwitcher failed")
+		logger.Error("RoundRobinProxySwitcher failed")
 	}
 	url := "https://google.com"
 	var f collect.Fetcher = collect.BrowserFetch{
@@ -23,20 +27,10 @@ func main() {
 
 	body, err := f.Get(url)
 	if err != nil {
-		fmt.Printf("read content failed:%v\n", err)
+		logger.Error("read content failed",
+			zap.Error(err),
+		)
 		return
 	}
-	fmt.Println(string(body))
-
-	// 加载HTML文档
-	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(body))
-	if err != nil {
-		fmt.Println("read content failed:%v", err)
-	}
-
-	doc.Find("div.news_li h2 a[target=_blank]").Each(func(i int, s *goquery.Selection) {
-		// 获取匹配元素的文本
-		title := s.Text()
-		fmt.Printf("Review %d: %s\n", i, title)
-	})
+	logger.Info("get content", zap.Int("len", len(body)))
 }

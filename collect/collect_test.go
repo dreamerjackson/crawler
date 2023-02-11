1e03b82e379b4bab9a18bf7c293e0a00146c50f4
letterbee <unlimitedzazp@outlook.com>
2023-02-11 10:50:38 +0800

    add testcase about of timeout param

--- collect/collect_test.go
+++ collect/collect_test.go
@@ -2,6 +2,7 @@ package collect
 
 import (
 	"testing"
+	"time"
 
 	"github.com/dreamerjackson/crawler/spider"
 	"github.com/stretchr/testify/assert"
@@ -27,5 +28,21 @@ func Test_BrowserFetch(t *testing.T) {
 	f := BrowserFetch{}
 	body, err := f.Get(req)
 	assert.Nil(t, err)
+	t.Log(string(body))
+}
+
+func Test_BrowserFetchWithTimeout(t *testing.T) {
+	url := "https://book.douban.com/subject/1007305/"
+	req := &spider.Request{
+		URL: url,
+	}
+	f := BrowserFetch{
+		Timeout: 300 * time.Millisecond,
+	}
+	body, err := f.Get(req)
+	if err != nil {
+		// log err info : timeout exceed
+		t.Log(err.Error())
+	}
 	t.Log(string(body))
 }
